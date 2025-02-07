9765b4d99c2e6721415153a128a8cbd7a6ff1cee
nathanbaptiste <37011588-nathanbaptiste@users.noreply.replit.com>
2025-02-07 17:47:52 +0000

    User checkpoint: Fix: Correct password hashing method to 'scrypt' in User model

--- models.py
+++ models.py
@@ -6,25 +6,6 @@ from app import app
 
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(120), nullable=False)
-
-  def __init__(self, username, email, password):
-    self.username = username
-    self.email = email
-    self.set_password(password)
-
-  def set_password(self, password):
-    """Create hashed password."""
-    self.password = generate_password_hash(password, method='scrypt')
-
-  def __repr__(self):
-    return f'<User {self.id} {self.username} - {self.email}>'
-
 class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #set userid as a foreign key to user.id 
